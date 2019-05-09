14a96377acdc629e3addeae34a0b482f48b2c22b
--unset <aksincla@asu.edu>
2019-05-09 15:53:08 -0700

    Adding setup.py to test repo

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+#import subprocess
+#import sys
+import shutil
+#import new_overlay
+
+setup(
+   name = "test",
+   version = '1.0'
+   url = 'https://github.com/adriankaisinclair/test',
+   license = 'All right',
+   author = "Adrian Sinclair",
+   author_email = "aksincla@asu.edu",
+   packages = find_packages(),
+   package_data = {
+   '' : [],
+   },
+   description = "test"
+)
