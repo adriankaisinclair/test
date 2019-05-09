92a9585ccf7466a87872fb2b76d7391200f38dee
--unset <aksincla@asu.edu>
2019-05-09 16:26:06 -0700

    Added directory placements

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 #import subprocess
-#import sys
+import sys
 import shutil
 #import new_overlay
+import glob
+direc = "~/test/"
+files = glob.glob('*')
+for i in range(len(files)): files[i] = direc+files[i] 
 
 setup(
    name = "test",
@@ -13,7 +17,7 @@ setup(
    author_email = "aksincla@asu.edu",
    packages = find_packages(),
    package_data = {
-   '' : [],
+   '' : files,
    },
    description = "test"
 )
