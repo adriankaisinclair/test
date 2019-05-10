e9e0d83b425340626f9bdbe9f2f6c63c40b34e65
--unset <aksincla@asu.edu>
2019-05-10 11:15:53 -0700

    adding file types

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup, find_packages
-#import subprocess
 import sys
 import shutil
-#import new_overlay
 import glob
 direc = "home/xilinx/"
 files = glob.glob('*')
-for i in range(len(files)): files[i] = direc+files[i] 
+#for i in range(len(files)): files[i] = direc+files[i] 
 
 setup(
    name = "test",
@@ -15,10 +13,10 @@ setup(
    license = 'All right',
    author = "Adrian Sinclair",
    author_email = "aksincla@asu.edu",
-   packages = find_packages(),
+   packages = [],
    package_data = {
-   '' : [],
+   '' : ['*.txt','*.md'],
    },
-   data_files = [files],
+   #data_files = [('../stuff',files)],
    description = "test"
 )
